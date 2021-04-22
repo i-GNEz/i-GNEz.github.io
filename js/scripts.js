c5ae0997eb7682ff221dbcfa435d1446b4280800
ahampriyanshu <ahampriyanshu@gmail.com>
2021-04-22 08:03:13 +0530

    landing page => events added

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,72 @@
 var h = document.documentElement,
-b = document.body,
-st = 'scrollTop',
-sh = 'scrollHeight',
-progress = document.querySelector('#progress'),
-scroll;
+    b = document.body,
+    st = 'scrollTop',
+    sh = 'scrollHeight',
+    progress = document.querySelector('#progress'),
+    scroll;
 var scrollpos = window.scrollY;
 var header = document.getElementById("header");
 var navcontent = document.getElementById("nav-content");
 
-document.addEventListener('scroll', function() {
+document.addEventListener('scroll', function () {
 
-/*Refresh scroll % width*/
-scroll = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
-progress.style.setProperty('--scroll', scroll + '%');
+    /*Refresh scroll % width*/
+    scroll = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+    progress.style.setProperty('--scroll', scroll + '%');
 
-/*Apply classes for slide in bar*/
-scrollpos = window.scrollY;
+    /*Apply classes for slide in bar*/
+    scrollpos = window.scrollY;
 
-if (scrollpos > 10) {
-    header.classList.add("bg-black-555");
-    navcontent.classList.remove("bg-black-555");
-    navcontent.classList.add("bg-black-555");
-} else {
-    header.classList.remove("bg-black-555");
-    navcontent.classList.remove("bg-black-555");
-    navcontent.classList.add("bg-black-555");
+    if (scrollpos > 10) {
+        header.classList.add("bg-black-555");
+        navcontent.classList.remove("bg-black-555");
+        navcontent.classList.add("bg-black-555");
+    } else {
+        header.classList.remove("bg-black-555");
+        navcontent.classList.remove("bg-black-555");
+        navcontent.classList.add("bg-black-555");
 
-}
+    }
 
 });
 
 
 //Javascript to toggle the menu
-document.getElementById('nav-toggle').onclick = function() {
-document.getElementById("nav-content").classList.toggle("hidden");
+document.getElementById('nav-toggle').onclick = function () {
+    document.getElementById("nav-content").classList.toggle("hidden");
 }
+
+var events = ["Workshop", "Code Relay", "Hackathon", "Electrohack"];
+var departments = ["Mechanical Engineering", "Computer Science Engineering", "Computer Science Engineering", "Electronics Engineering"];
+var totalLayout = 4
+
+function createSlide() {
+    var e = document.getElementById("slidesDiv");
+    for (var t = 1; t <= totalLayout; t++) {
+        var a = document.createElement("div");
+        a.setAttribute("class", "mySlides hidden"), e.appendChild(a);
+        var n = document.createElement("img");
+        n.setAttribute("src", "images/" + t + ".png"), n.setAttribute("class", "description w-full object-cover"), n.setAttribute("alt", "Event" + t), n.setAttribute("onclick", "currentSlide(" + t + ")"), a.appendChild(n)
+    }
+}
+createSlide();
+var slideIndex = 1;
+
+function plusSlides(e) {
+    showSlides(slideIndex += e)
+}
+
+function currentSlide(e) {
+    showSlides(slideIndex = e)
+}
+
+function showSlides(e) {
+    var t, a = document.getElementsByClassName("mySlides"),
+        n = document.getElementsByClassName("description"),
+        i = document.getElementById("event");
+        j = document.getElementById("department");
+    for (e > a.length && (slideIndex = 1), e < 1 && (slideIndex = a.length), t = 0; t < a.length; t++) a[t].style.display = "none";
+    for (t = 0; t < n.length; t++) n[t].className = n[t].className.replace(" opacity-100", "");
+    a[slideIndex - 1].style.display = "block", n[slideIndex - 1].className += " opacity-100", i.innerHTML = events[slideIndex-1], j.innerHTML = departments[slideIndex-1]
+}
+showSlides(slideIndex);
