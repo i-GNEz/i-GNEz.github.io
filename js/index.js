49f7e3f26b406ea2285a33cddb53ea6df6771ab6
ahampriyanshu <ahampriyanshu@gmail.com>
2021-04-30 04:10:45 +0530

    trying a diffrent service worker

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../sw.js')
-    .then(reg => console.log('service worker registered', reg))
-    .catch(err => console.log('service worker not registered', err));
-}
-
 var h = document.documentElement,
     b = document.body,
     st = 'scrollTop',
