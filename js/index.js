e3f5cd1302a23fa7faa288542e24a14972d55c5a
ahampriyanshu <ahampriyanshu@gmail.com>
2021-04-30 00:48:26 +0530

    manifest.json

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('sw.js')
+//     .then(reg => console.log('service worker registered', reg))
+//     .catch(err => console.log('service worker not registered', err));
+// }
+
 var h = document.documentElement,
     b = document.body,
     st = 'scrollTop',
