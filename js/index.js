f03b5af43b2d02fc4cc4721ce3bac875e8f1338d
ahampriyanshu <ahampriyanshu@gmail.com>
2021-05-02 00:39:47 +0530

    fixing a typo

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ $('#footerForm').submit(function (e) {
         statusCode: {
             429: function (response) {
                 openClose('emailError', 'Permitted Limit Exhausted', 'Hey techie, your Permitted limit has been exhausted. Please try again after some time');
-                emailBtn.remove;
+                emailBtn.remove();
             },
             200: function (response) {
                 if (response.message == 'No event registered') {
